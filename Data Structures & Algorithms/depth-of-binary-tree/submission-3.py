c4756fcfd19bfab981e390281da996e4e4fb3185
Sahana Arumugam <sahanaarums16@gmail.com>
2026-07-25 20:27:48 +0530

    Add: depth-of-binary-tree - submission-3

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-3.py
@@ -0,0 +1,67 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack=[[root,1]]
+        res=0
+
+        while stack:
+            node,depth = stack.pop()
+            if node:
+                res=max(res,depth)
+                stack.append([node.right , depth+1])
+                stack.append([node.left , depth+1])
+        return res
+
+
+
+
+
+
+
+#DFS - Recursive
+
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         return 1 + max(self.maxDepth(root.left) , self.maxDepth(root.right))
+
+
+#BFS - Iterative
+
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+#         q=deque([root])
+#         level=0
+#         while q:
+#             for i in range(len(q)):
+#                 node=q.popleft()
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
+#             level+=1
+#         return level
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
